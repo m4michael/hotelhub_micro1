6bea4737d973ebbd01b9d9d4587d8a20890ae975
Michael Miki Murmu <114741392+m4michael@users.noreply.github.com>
2024-07-03 08:39:05 +0530

    Add files via upload

---
+++ HotelHub-configServer/src/test/java/com/hotel/configServer/HotelHubConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelHubConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
